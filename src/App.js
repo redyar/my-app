8fe624f7c9675a38dd2a1f7248cc551f92a17ea3
redyar <winterisred@gmail.com>
2022-03-09 22:40:53 +0400

    Dec and Inc + -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  let [likes, setlikes] = useState(2)
+  
+  function inc(){
+    setlikes(likes += 1);
+  }
+  function dec(){
+    setlikes(likes -= 1);
+  }
   return (
     <div className="App">
-
+      <h1>{likes}</h1>
+      <button onClick={dec} >Decrement</button>
+      <button onClick={inc} >Increment</button>
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
 
 ReactDOM.render(
-
-);
+    <div>
+      Work!
+      <button onClick={() => console.log('HiMyLove')}>Bytton</button>
+      <App/>
+    </div>,
+    document.getElementById('root')
+  ) 
