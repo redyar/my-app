b9f4f6df019cab42dd4030253f9cf03bd32ee007
redyar <winterisred@gmail.com>
2022-03-09 23:06:27 +0400

    add Components, Counter and Input

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, {useState} from 'react';
+import Counter from './components/Counter';
+import Input from './components/Input';
 
 function App() {
 
-  let [likes, setlikes] = useState(2);
-  let [value, settext] = useState('Text');
 
-  function changeFn(e){
-    settext(e.target.value)
-  }
+
 
   
-  function inc(){
-    setlikes(likes += 1);
-  }
-  function dec(){
-    setlikes(likes -= 1);
-  }
+
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h2>{value}</h2>
-      <input type='text' value={value} onChange={changeFn}></input>
-      <button onClick={dec} >Decrement</button>
-      <button onClick={inc} >Increment</button>
+      <Input/>
+
+      <Counter/>
+      <Counter/>
     </div>
   ); 
 }
