345964ea0d7672583410c64a7ac600f61efc0599
redyar <winterisred@gmail.com>
2022-03-09 22:52:40 +0400

    SupperInput

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, {useState} from 'react';
 
 function App() {
 
-  let [likes, setlikes] = useState(2)
+  let [likes, setlikes] = useState(2);
+  let [value, settext] = useState('Text');
+
+  function changeFn(e){
+    settext(e.target.value)
+  }
+
   
   function inc(){
     setlikes(likes += 1);
@@ -13,6 +19,8 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input type='text' value={value} onChange={changeFn}></input>
       <button onClick={dec} >Decrement</button>
       <button onClick={inc} >Increment</button>
     </div>
