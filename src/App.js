ba717c3dd1981a9f2be6a622d806eef0ec173a15
redyar <winterisred@gmail.com>
2022-03-10 12:50:06 +0400

    something

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Counter from './components/Counter';
 import Input from './components/Input';
 import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
 
 function App() {
 
@@ -12,6 +13,18 @@ function App() {
       <Counter/>
       <h3>Counter на основе Класса</h3>
       <ClassCounter/>
+      <hr/>
+      <div className='post'>
+        <div className='post__content'>
+          <strong>1. JS</strong>
+          <div>
+            For JS is next.js
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button>Delete</button>  
+        </div>  
+      </div>
     </div>
   ); 
 }
