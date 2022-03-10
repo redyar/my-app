21725e2a94e827ae85634f7e2f1769460828552d
redyar <winterisred@gmail.com>
2022-03-10 07:54:40 +0400

    ClassCounter компонент на основе класса

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, {useState} from 'react';
 import Counter from './components/Counter';
 import Input from './components/Input';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
-
-
-
-
-  
 
   return (
     <div className="App">
       <Input/>
-
+      <h3>Counter на основе Хукков</h3>
       <Counter/>
-      <Counter/>
+      <h3>Counter на основе Класса</h3>
+      <ClassCounter/>
     </div>
   ); 
 }
 
 export default App;
+ 
