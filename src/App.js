119ae389620e7e6198301cf476e2d2384e6166a6
redyar <winterisred@gmail.com>
2022-03-13 00:15:38 +0400

    ss

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, {useState} from 'react';
 import Counter from './components/Counter';
 import Input from './components/Input';
 import ClassCounter from './components/ClassCounter';
-import './styles/App.css';
-import ClassItem from './components/ClassItem';
+import './styles/App.css'
+import PostList from './components/PostList';
 
 
 function App() {
+
 
   return (
     <div className="App">
@@ -16,10 +17,8 @@ function App() {
       <h3>Counter на основе Класса</h3>
       <ClassCounter/>
       <hr/>
-      <ClassItem post={{id:1 } }/>
-      
-      <ClassItem  />
-      <ClassItem />
+      <PostList/>
+
     </div>
   ); 
 }
